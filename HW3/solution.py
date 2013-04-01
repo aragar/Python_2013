433d012bf43e89016a752de969b18c2be763dc21
Radoslav Rachev <radorachev@gmail.com>
2013-04-01 15:22:34 +0300

    Update solution.py
    Some changes because of the last minute qustion's answers on the forum.

--- HW3/solution.py
+++ HW3/solution.py
@@ -10,28 +10,28 @@ class Person:
 
         self._children = list()
 
-        for parent in self.__get_known_parents():
+        for parent in self._get_known_parents():
         	parent._add_child(self)
 
     def _add_child(self, child):
         self._children.append(child)
 
-    def __get_known_parents(self):
+    def _get_known_parents(self):
         return [parent for parent in [self.mother, self.father] if
                 parent is not None]
 
-    def __get_siblings(self, gender):
+    def _get_siblings(self, gender):
         siblings = set()
-        for parent in self.__get_known_parents():
+        for parent in self._get_known_parents():
             siblings |= set(parent.children(gender))
         siblings -= {self}
         return siblings
 
     def get_brothers(self):
-        return list(self.__get_siblings('M'))
+        return list(self._get_siblings('M'))
 
     def get_sisters(self):
-        return list(self.__get_siblings('F'))
+        return list(self._get_siblings('F'))
 
     def children(self, gender=None):
         if gender:
