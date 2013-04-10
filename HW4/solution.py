c5b7597a75ca68318e4d990b0c25af43992d1be3
Radoslav Rachev <radorachev@gmail.com>
2013-04-11 01:12:25 +0300

    HW4 - Exceptions implementation
    The four exceptions are implemented

--- HW4/solution.py
+++ HW4/solution.py
@@ -1,13 +1,36 @@
+class InvalidMove(Exception):
+    pass
+
+class InvalidValue(Exception):
+    pass
+
+class InvalidKey(Exception):
+    pass
+
+class NotYourTurn(Exception):
+    pass
+
 class TicTacToeBoard:
 
     def __init__(self):
         self.board = dict()
+        self._last = 'RADO'
 
     def __getitem__(self, key):
         print(self.board[key])
 
     def __setitem__(self, key, value):
-        self.board[key] = value
+        if key in self.board:
+            raise InvalidMove
+        elif key not in [column + row for row in '123' for column in 'ABC']:
+            raise InvalidKey
+        elif value not in ['X', 'O']:
+            raise InvalidValue
+        elif value == self._last:
+            raise NotYourTurn
+        else:
+            self.board[key] = value
+            self._last = value
 
     def __str__(self):
         return ('\n' +\
