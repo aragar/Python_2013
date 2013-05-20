8a736dc364d980f61a85c719ce049ccc848d3e47
Radoslav Rachev <radorachev@gmail.com>
2013-05-20 23:51:16 +0300

    Reversi Board init

---
+++ Project/solution.py
@@ -0,0 +1,36 @@
+class InvalidMove(Exception):
+    pass
+
+
+class InvalidValue(Exception):
+    pass
+
+
+class InvalidKey(Exception):
+    pass
+
+
+class NotYourTurn(Exception):
+    pass
+
+
+class ReversiBoard:
+	
+
+    def __init__(self):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def update_game_status(self):
+        pass
+
+    def __str__(self):
+    	pass
+
+    def game_status(self):
+        pass
