3c22686b59cef8c0aa2e8052daeebe7d7905dc80
Radoslav Rachev <radorachev@gmail.com>
2013-06-22 01:50:16 +0300

    Additional invalid moves are added
    From the rules of the game, a piece can be placed only if there is a
    straight line between the new piece and an anchoring piece with the same
    color containing at least one piece of the opponent color.
    
    See [the Rules](http://en.wikipedia.org/wiki/Reversi#Rules)

--- Project/ReversiBoard.py
+++ Project/ReversiBoard.py
@@ -24,38 +24,84 @@ class ReversiBoard:
 
     class ReversiBoardLine:
 
-        def __init__(self, board):
+        def __init__(self, board, row):
             self.board = board
-            self.boardLine = dict()
+            self.row = row
+            self.board_line = dict()
+
+            if row == 3:
+                self.board_line[3] = ReversiBoard.WHITE
+                self.board_line[4] = ReversiBoard.BLACK
+            elif row == 4:
+                self.board_line[3] = ReversiBoard.BLACK
+                self.board_line[4] = ReversiBoard.WHITE
 
         def __getitem__(self, key):
             if key not in range(0, ReversiBoard.BOARD_SIZE):
                 raise InvalidKey
-            return self.boardLine.get(key, '')
+            return self.board_line.get(key, '')
 
         def __setitem__(self, key, value):
             if key not in range(0, ReversiBoard.BOARD_SIZE):
                 raise InvalidKey
-            elif key in self.boardLine:
+            elif key in self.board_line:
                 raise InvalidMove
             elif value not in ReversiBoard.PLAYERS:
                 raise InvalidValue
             elif value == self.board.last_move:
                 raise NotYourTurn
+            elif (self.row, key) not in self.board.get_possible_moves(value):
+               raise InvalidMove
             else:
-                self.boardLine[key] = value
+                self.board_line[key] = value
                 self.board.last_move = value
 
     def __init__(self):
-        self.board = [self.ReversiBoardLine(self) for _ in range(0, self.BOARD_SIZE)]
-        self.last_move = self.WHITE # Blacks are first to play
-
-        self.board[3][4] = self.BLACK
-        self.board[3][3] = self.WHITE
-        self.board[4][3] = self.BLACK
-        self.board[4][4] = self.WHITE
+        self.board = [self.ReversiBoardLine(self, row)
+                      for row
+                      in range(0, self.BOARD_SIZE)]
+        self.last_move = self.WHITE  # Blacks are first to play
 
     def __getitem__(self, key):
         if key not in range(0, self.BOARD_SIZE):
             raise InvalidKey
         return self.board[key]
+
+    def get_possible_moves(self, player):
+        possible_moves = [(x, y)
+                          for x in range(0, self.BOARD_SIZE)
+                          for y in range(0, self.BOARD_SIZE)
+                          if self.check_move_possible(x, y, player)]
+
+        return possible_moves
+
+    def check_move_possible(self, x, y, player):
+        if x not in range(0, self.BOARD_SIZE) or y not in range(0, self.BOARD_SIZE):
+            return False
+
+        if player not in self.PLAYERS:
+            return False
+
+        if self.board[x][y]:
+            return False
+
+        DX = [1, 1, 0, -1, -1, -1, 0, 1]
+        DY = [0, -1, -1, -1, 0, 1, 1, 1]
+
+        for delta in range(0, self.BOARD_SIZE):
+            new_x = x + DX[delta]
+            new_y = y + DY[delta]
+            while (new_x in range(0, self.BOARD_SIZE) and
+                   new_y in range(0, self.BOARD_SIZE) and
+                   self.board[new_x][new_y] and
+                   self.board[new_x][new_y] != player):
+                new_x += DX[delta]
+                new_y += DY[delta]
+
+            if (new_x in range(0, self.BOARD_SIZE) and
+                new_y in range(0, self.BOARD_SIZE) and
+                self.board[new_x][new_y] == player and
+                    (new_x - x != DX[delta] or new_y - y != DY[delta])):
+                return True
+
+        return False
--- Project/test.py
+++ Project/test.py
@@ -11,7 +11,7 @@ class TestReversiBoard(unittest.TestCase):
     def test_invalid_value_exception_raises(self):
         invalid_value = ReversiBoard()
         with self.assertRaises(InvalidValue):
-            invalid_value[1][1] = "F"
+            invalid_value[2][3] = "F"
 
     def test_invalid_key_exception_raises(self):
         invalid_key = ReversiBoard()
@@ -27,8 +27,26 @@ class TestReversiBoard(unittest.TestCase):
     def test_not_your_turn_exception_raises(self):
         not_your_turn = ReversiBoard()
         with self.assertRaises(NotYourTurn):
-            not_your_turn[0][0] = "X"
-            not_your_turn[0][1] = "X"
+            not_your_turn[2][3] = "X"
+            not_your_turn[5][2] = "X"
+
+    def test_possible_moves(self):
+        board = ReversiBoard()
+        possible_moves = board.get_possible_moves(ReversiBoard.BLACK)
+        self.assertEqual(possible_moves, [(2, 3), (3, 2), (4, 5), (5, 4)])
+
+    def test_check_move_possible(self):
+        board = ReversiBoard()
+        self.assertTrue(board.check_move_possible(2, 3, ReversiBoard.BLACK))
+
+    def test_check_move_not_possible(self):
+        board = ReversiBoard()
+        self.assertFalse(board.check_move_possible(1, 1, ReversiBoard.BLACK))        
+
+    def test_invalid_move_exception_raises_move_not_possible(self):
+        move_not_possible = ReversiBoard()
+        with self.assertRaises(InvalidMove):
+            move_not_possible[1][1] = ReversiBoard.BLACK
 
 if __name__ == '__main__':
     unittest.main()
