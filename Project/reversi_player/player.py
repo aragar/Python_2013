ff56f25e1c60fd828b5e5cd33be1ec210d92aafd
Radoslav Rachev <radorachev@gmail.com>
2013-06-27 16:46:00 +0300

    Player class
    The players are going to be placed in separate classes.

---
+++ Project/reversi_player/player.py
@@ -0,0 +1,11 @@
+from reversi_board.reversi_board import ReversiBoard
+
+
+class Player:
+
+    def __init__(self, board, color):
+        self._board = board
+        self._color = color
+
+    def move(self):
+        pass
